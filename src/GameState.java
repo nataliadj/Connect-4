9aa6073e0e841bacf3557f69208f639bccdaf1bc
davinkurniawan <davinkurniawan07@gmail.com>
2015-05-10 23:05:10 +1000

    gamestate

--- src/GameState.java
+++ src/GameState.java
@@ -2,29 +2,52 @@ import java.util.ArrayList;
 
 
 public class GameState {
+	
 	private ArrayList<ArrayList<Integer>> board; 
 	private int player;
 	
-	public GameState() {
-		//TODO
+	/**
+	 * preconditions : player == 0 || player == 1
+	 * @param player : int
+	 */
+	public GameState(int player) {
+		this.player = player;
+		this.board = new ArrayList<ArrayList<Integer>>();
 	}
 	
-	public void add(int column, int player) {
-		//TODO
+	/**
+	 * preconditions : 	col >= 0 && col <= 6
+	 * 					player == 0 || player == 1
+	 * @param col : int
+	 * @param player : int
+	 */
+	public void add(int col, int player) {
+		board.get(col).add(player);
 	}
 	
+	/**
+	 * @return int
+	 */
 	public int getPlayer() {
-		//TODO
 		return player;
 	}
 	
+	/**
+	 * @return ArrayList<ArrayList<Integer>>
+	 */
 	public ArrayList<ArrayList<Integer>> getBoard(){
-		//TODO
 		return board;
 	}
 	
-	public boolean isFull(int column) {
-		//TODO
+	/**
+	 * precondition : col >= 0 && col <= 6
+	 * @param col : int
+	 * @return boolean
+	 */
+	public boolean isFull(int col) {
+		if(board.get(col).size() == 6) {
+			return true;
+		}
 		return false;
 	}
 
