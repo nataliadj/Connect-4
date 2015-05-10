91de297e554a437285466d52effaa401e130eb38
davinkurniawan <davinkurniawan07@gmail.com>
2015-05-10 23:04:09 +1000

    Revert "gamestate"
    This reverts commit 5d1f79a0417982aab9b2e4a71f4af5d5b47dcd41.

--- src/GameState.java
+++ src/GameState.java
@@ -2,52 +2,29 @@ import java.util.ArrayList;
 
 
 public class GameState {
-	
 	private ArrayList<ArrayList<Integer>> board; 
 	private int player;
 	
-	/**
-	 * preconditions : player == 0 || player == 1
-	 * @param player : int
-	 */
-	public GameState(int player) {
-		this.player = player;
-		this.board = new ArrayList<ArrayList<Integer>>();
+	public GameState() {
+		//TODO
 	}
 	
-	/**
-	 * preconditions : 	col >= 0 && col <= 6
-	 * 					player == 0 || player == 1
-	 * @param col : int
-	 * @param player : int
-	 */
-	public void add(int col, int player) {
-		board.get(col).add(player);
+	public void add(int column, int player) {
+		//TODO
 	}
 	
-	/**
-	 * @return int
-	 */
 	public int getPlayer() {
+		//TODO
 		return player;
 	}
 	
-	/**
-	 * @return ArrayList<ArrayList<Integer>>
-	 */
 	public ArrayList<ArrayList<Integer>> getBoard(){
+		//TODO
 		return board;
 	}
 	
-	/**
-	 * precondition : col >= 0 && col <= 6
-	 * @param col : int
-	 * @return boolean
-	 */
-	public boolean isFull(int col) {
-		if(board.get(col).size() == 6) {
-			return true;
-		}
+	public boolean isFull(int column) {
+		//TODO
 		return false;
 	}
 
