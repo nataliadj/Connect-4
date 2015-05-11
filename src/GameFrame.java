8b6039184d2fac86510c04b820250b4a575a1d46
gabyfebriana <gabriela_febriana@yahoo.com>
2015-05-12 00:34:43 +1000

    GameFrame
    grids

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,16 +1,19 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 
 public class GameFrame extends JFrame {
-
-	public GameFrame (String title) {
+	/*public GameFrame (String title) {
 		super(title);
 	
 	// Set layout manager
@@ -35,7 +38,29 @@ public class GameFrame extends JFrame {
 			
 		}
 		
-	});
-	
-	}
+	});*/
+	public GameFrame(String title){
+		setLayout(new BorderLayout());
+		JPanel board = new JPanel();  
+        board.setLayout(new GridLayout(7, 6)); 
+        Square[][] boardGUI = new Square[7][6];
+      
+        for (int i=0; i<7; i++) { 
+            for (int j = 0; j <6; j++) {
+               boardGUI[i][j] = new Square(i, j);
+               board.add(boardGUI[i][j]);  
+               //TODO adding mouse event
+            }
+        }  
+		JPanel buttonPanel = new JPanel();
+	    buttonPanel.setLayout(new GridLayout(10,1));
+		
+		JButton startButton = new JButton("New Game");
+		JButton undoButton = new JButton("Undo");
+		buttonPanel.add(startButton);
+		buttonPanel.add(undoButton);
+
+		add(board, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.EAST);
+	}  
 }
---
+++ src/Square.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+
+public class Square extends JPanel {   
+	private int row, column;
+	public int value;
+	public Square(int r, int c) {
+		this.row = r;
+		this.column = c;
+		this.value = -1;
+		this.setBackground(Color.BLUE);	
+		this.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+	}
+	public int getColumn() {
+		return column;
+	}
+	public int getRow() {
+		return row;
+	}
+	public void setValue(int v) {
+		this.value = v;
+	}
+}
