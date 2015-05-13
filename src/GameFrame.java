4e49deeb67cbefea65edeb5c08a6bbcc970f11fb
NataliaDj <nataliadjohari@yahoo.com.sg>
2015-05-13 16:31:48 +1000

    Modified new game button
    commented possible menubar
    deleted undo button

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,16 +2,19 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 
 public class GameFrame extends JFrame {
+	private int gameType = 0;
+
+	private GameEngine ge;
 	/*public GameFrame (String title) {
 		super(title);
 	
@@ -39,29 +42,45 @@ public class GameFrame extends JFrame {
 		
 	});*/
 	public GameFrame(String title){
+		super (title);
+		
 		setLayout(new BorderLayout());
 		JPanel board = new JPanel();  
         board.setLayout(new GridLayout(7, 6)); 
-        Square[][] boardGUI = new Square[7][6];
+        final Square[][] boardGUI = new Square[7][6];
       
         for (int i=0; i<7; i++) { 
             for (int j = 0; j <6; j++) {
                boardGUI[i][j] = new Square(i, j);
                board.add(boardGUI[i][j]);  
-               //TODO adding mouse event
+               
+              
             }
-        }  
+        }
+        
+        /*JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        JMenuItem newGame = new JMenuItem ("New Game");
+        fileMenu.add(newGame);
+        newGame.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//String name = JOptionPane.showInputDialog(new JFrame(), "What is your name?", null);
+				Object[] options = {"Human vs Human", "Human vs Computer"};
+				String choice = (String)JOptionPane.showInputDialog(null,"Choose new game type",
+						"New Game",JOptionPane.QUESTION_MESSAGE,null,options,"Human vs Human");
+			}
+			
+		});*/
+        
 		JPanel buttonPanel = new JPanel();
 	    buttonPanel.setLayout(new GridLayout(10,1));
 		
 		JButton startButton = new JButton("New Game");
-		//JButton undoButton = new JButton("Undo");
 		buttonPanel.add(startButton);
-		//buttonPanel.add(undoButton)
-		/*JTextArea textArea = new JTextArea ("Connect 4");
-		/JScrollPane scroll = new JScrollPane (textArea, 
-				   JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		buttonPanel.add(scroll);*/
 
 		add(board, BorderLayout.CENTER);
 		add(buttonPanel, BorderLayout.EAST);
@@ -71,11 +90,27 @@ public class GameFrame extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//String name = JOptionPane.showInputDialog(new JFrame(), "What is your name?", null);
+
+				//initialize game type
 				Object[] options = {"Human vs Human", "Human vs Computer"};
 				String input = (String)JOptionPane.showInputDialog(null,"Choose new game type",
 						"New Game",JOptionPane.QUESTION_MESSAGE,null,options,"Human vs Human");
+				
+				if (input.equals("Human vs Human")) {
+					gameType = 1;
+				} else {
+					gameType = 0;
+				}
+				
+				//initialize new game
+				for (int i=0; i<7; i++) { 
+		            for (int j = 0; j <6; j++) {
+		               boardGUI[i][j].setValue(-1);
+		            }
+		        }
 			}
 			
 		});
+		
 	}  
 }
