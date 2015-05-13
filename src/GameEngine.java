d4e3157e851038b60c93454cdd10673a2dac66c5
ExaQuaTrZ <z5017140@zmail.unsw.edu.au>
2015-05-13 15:11:18 +1000

    Update GameEngine.java

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,29 +1,92 @@
 
+import java.util.ArrayList;
+
+
 public class GameEngine {
-	private GameState gs;
+	GameState gs;
+	AI computer;
 	
-	public GameEngine() {
-		this.gs = new GameState();
-		//TODO
+	//place constructor here
+	
+	/**
+	 * Checks if a move is valid
+	 * @param column	The column the move is inserting into
+	 * @return	The row to insert into if the move is valid, otherwise -1
+	 */
+	public int validMove(int column) {
+		ArrayList<ArrayList<Integer>> board = gs.getBoard();
+		int size = board.get(column).size();
+		if (size < 6) {
+			return size + 1;
+		}
+		return -1;
 	}
 	
-	public boolean validMove(int column) {
-		//TODO
-		return false;
-		
-	}
-	
+	/**
+	 * makes a move
+	 * PRECONDITION: column >= 0 && column < 7
+
+	 * @param column	The column to add to
+	 */
 	public void makeMove(int column) {
-		//TODO
+		gs.add(column);
 	}
 	
-	public int callAI() {
-		//TODO
-		return 0;
+	/**
+	 * placeholder
+	 * @return
+	 */
+	public int callAi() {
+		return computer.decideMove(gs);
 	}
 	
-	public boolean winCond() {
-		//TODO
-		return false;
+	/**
+	 * 
+	 * PRECONDITION: column >= 0 && column < 7
+	 * @param column
+	 * @param player	Player whom made the move being checked
+	 * @return
+	 */
+	public boolean winCond(int column, int player) {
+		ArrayList<ArrayList<Integer>> board = gs.getBoard();
+		ArrayList<Integer> columnImp = board.get(column);
+		int row = board.get(column).size() + 1;
+		
+		//check vertical
+		if (row >= 3) {
+			if ((columnImp.get(row-1) == columnImp.get(row-2)) 
+					&& (columnImp.get(row-2) == columnImp.get(row-3))) {
+				return true;
+			}
+		}
+		
+		//check horizontal
+		int horizontalFind = 0;
+		for (int col = 0; col < 7; col++) {
+			if (board.get(col).get(row) == player) {
+				horizontalFind ++;
+			} else {
+				horizontalFind = 0;
+			}
+			
+			if (horizontalFind == 4) {
+				return true;
+			}
+		}
+		
+		int tempRow1 = row;	//for "/"
+		int tempRow2 = row;	//for "\"
+		int diagFindOne = 0;
+		int diagFindTwo = 0;
+		int break1;
+		int break2;
+		for (int col = column; col >= 0; col--) {
+			
+			
+			
+			tempRow1--;
+			tempRow2++;
+		}
 	}
 }
+
