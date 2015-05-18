37e5e2419fde54d5f56943dbb017fb52ac11c84d
davinkurniawan <davinkurniawan07@gmail.com>
2015-05-18 15:55:11 +1000

    Revert "moved winCond to gamestate, so ai can use it"
    This reverts commit 05a020b321002c195a28ca39b07dde07aea11c44.

--- src/AI.java
+++ src/AI.java
@@ -1,13 +1,14 @@
-import java.util.*;
+import java.util.Random;
 
 
 public class AI implements AIInterface{
-	//private int prevNum = 0;
+	private int prevNum = 0;
 
-	@Override
 	public int decideMove(GameState gs) {
-		
-		
+		Random rand = new Random();
+		int randomNum = rand.nextInt((5) + 1);
+		prevNum = randomNum;
+		return randomNum;
 	}
 
 }
---
+++ src/MainMenuPanel.java
@@ -0,0 +1,8 @@
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+public class MainMenuPanel extends JPanel {
+}
