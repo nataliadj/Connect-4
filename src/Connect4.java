de0856861388d74d31eac54003715ed7c719ab1a
NataliaDj <nataliadjohari@yahoo.com.sg>
2015-05-26 16:07:32 +1000

    Enable Play

--- src/Connect4.java
+++ src/Connect4.java
@@ -11,12 +11,12 @@ public class Connect4 {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				JFrame frame = new JFrame ("Connect 4");
+				JFrame frame = new GameFrame ("Connect 4");
 				frame.setSize(900,600);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);		
 				frame.setLayout(new BorderLayout());
-				frame.add(new MenuPanel());
+				//frame.add(new MenuPanel());
 			}
 		});
 	}
